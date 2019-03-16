ef26bebf939b4c7ca22263108769861cdc43a0e8
rflorian2017 <roby_florian@yahoo.com>
2019-03-16 15:25:01 +0200

    Tim3 -> recapitulare

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -164,15 +164,16 @@ public class Main {
         }
         //System.out.println(myNumber);
     }
+
     public static void pasareasca(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Introduceti text:");
         String text = scanner.next();
         System.out.println(text);
-        char[] vocals = new char[] {'a','e','i','o','u'};
+        char[] vocals = new char[]{'a', 'e', 'i', 'o', 'u'};
         for (int i = 0; i < text.length(); i++) {
             for (int j = 0; j < vocals.length; j++) {
-                if(vocals[j] == text.charAt(i)) {
+                if (vocals[j] == text.charAt(i)) {
 
                 }
             }
@@ -183,15 +184,14 @@ public class Main {
         String textToCode = "Dupa munca si rasplata! Lucrand cel putin 10 ore suplimentar pe saptamana, pot stapani programarea!";
         String codedText = "";
 
-        for(int i= 0; i< textToCode.length(); i++)
-        {
-            codedText += (char)(textToCode.charAt(i) + 10 - 7);
+        for (int i = 0; i < textToCode.length(); i++) {
+            codedText += (char) (textToCode.charAt(i) + 10 - 7);
         }
         System.out.println(codedText);
 
 
-        for (int k = 8; k <= 14; k = k+2) {
-            for (int j= 1 ; j < 10 ; j = j+2 ) {
+        for (int k = 8; k <= 14; k = k + 2) {
+            for (int j = 1; j < 10; j = j + 2) {
                 textToCode = "";
                 for (int i = 0; i < codedText.length(); i++) {
                     textToCode += (char) (codedText.charAt(i) - k + j);
@@ -202,7 +202,142 @@ public class Main {
 
     }
 
+    public static void main5(String[] args) {
+        int counter_4bits = 0;
+        int repetition = 0;
+
+        for ( /* start value */;
+            /* conditii */ ;
+            /* what happens after current iteration */) {
+            if (counter_4bits <= 15) {
+                //System.out.println(counter_4bits);
+                counter_4bits++;
+            } else {
+                counter_4bits = 0;
+                repetition++;
+            }
+
+            if (repetition == 3) {
+                break;
+            }
+        }
+
+        repetition = 0;
+        counter_4bits = 0;
+        while (repetition < 3) {
+
+            if (counter_4bits == 16) {
+                counter_4bits = 0;
+                repetition++;
+                continue;
+            }
+
+            //System.out.println(counter_4bits++);
+            counter_4bits++;
+        }
+
+        repetition = 0;
+        counter_4bits = 0;
+        do {
+            if (counter_4bits == 16) {
+                counter_4bits = 0;
+                repetition++;
+                continue;
+            }
+
+            System.out.println(counter_4bits++);
+        }
+        while (repetition < 3);
+
+    }
+
+    public static void main6(String[] args) {
+        String pare = "";
+        String impare = "";
+
+        for (int i = 0; i <= 30; i++) {
+            if (i % 2 == 0) {
+                pare += i + " ";
+            } else {
+                impare += i + " ";
+            }
+        }
+
+        System.out.println("Pare: " + pare + " ");
+        System.out.println("Impare: " + impare + " ");
+    }
+
+    public static void main7(String[] args) {
+        int a = 2;
+        Integer c = new Integer(257);
+        int[] b = new int[2];
+
+        MyClass class1 = new MyClass();
+        MyClass myClass2 = new MyClass();
+        //System.out.println(MyClass.numberOfInstances);
+        //System.out.println(myClass2.numberOfInstances);
+
+        System.out.println(a);
+        System.out.println(c.byteValue());
+        System.out.println((char) 96);
+
+    }
+
     public static void main(String[] args) {
+        int[] unidimensionarray = new int[10];
+
+        for (int i = 1; i < unidimensionarray.length + 1; i++) {
+            unidimensionarray[i - 1] = i;
+        }
+
+        System.out.print("unidimensionalarray = [");
+        for (int i = 0; i < unidimensionarray.length; i++) {
+            String textToPrint = "" + unidimensionarray[i];
+            textToPrint +=
+                    i == unidimensionarray.length - 1 ?
+                            "" :
+                            ", ";
+            System.out.print(textToPrint);
+        }
+        System.out.println("]");
+
+        int[][] bidimensionarray = new int[10][10];
+        int valoare = 0;
+        for (int row = 0; row < bidimensionarray.length; row++) {
+            for (int column = 0;
+                 column < bidimensionarray[row].length; column++) {
+                bidimensionarray[row][column] = valoare;
+                valoare++;
+            }
+        }
+
+        System.out.println("bidimensionarray :");
+        for (int row = 0; row < bidimensionarray.length; row++) {
+
+            for (int column = 0; column < bidimensionarray[row].length; column++) {
+                System.out.print(bidimensionarray[row][column] + " ");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println("bidimensionarray (2nd display method):");
+        System.out.print("[");
+        for (int row = 0; row < bidimensionarray.length; row++) {
+
+            System.out.print("[");
+            for (int column = 0; column < bidimensionarray[row].length; column++) {
+                if (column != bidimensionarray[row].length - 1)
+                    System.out.print(bidimensionarray[row][column] + ", ");
+                else
+                    System.out.print(bidimensionarray[row][column]);
+            }
+            if (row != bidimensionarray.length - 1)
+                System.out.print("],");
+            else
+                System.out.print("]");
+        }
+        System.out.println("]");
 
     }
 }
